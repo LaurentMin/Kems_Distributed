4b3361f30b0f869e4332beb32e588ca42eeb6782
Elouan Geffrelot <elouan.geffrelot@etu.utc.fr>
2024-05-08 11:59:02 +0200

    Added function to determine a seperation caracter given a key-value pair

--- ctl.go
+++ ctl.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 )
 
+///////////////////////
+// ENCODING MESSAGES //
+///////////////////////
 
 var fieldsep = "/"
 var keyvalsep = "="
+// Returns the first ASCII "seperator" character non present in the pair received as a parameter
+// Letters are not included (lookup order can be modified with beginRangeASCII and endRangeASCII)
+func determinePairSep(key string, val string) string{
+	msg := key + val
+	asciiVal := ""
+	// ASCII ranges in which to look for seperators (includes numbers)
+	beginRangeASCII := [5]int{58,33,91,123,48}
+	endRangeASCII := [5]int{64,47,96,126,57}
+	
+	// Error returns ""
+	if (len(beginRangeASCII) != len(endRangeASCII)) {
+		fmt.Println("Impossible to find seperation caracter (code error).")
+		return asciiVal
+	}
+	
+	// lookup loop (looks in the ascii ranges one by one if msg contains character)
+	for i := 0; i < len(beginRangeASCII); i++{
+		for  asciiCode := beginRangeASCII[i]; asciiCode <= endRangeASCII[i]; asciiCode++ {
+			asciiVal = string(rune(asciiCode))
+			if strings.Contains(msg, asciiVal) {
+				continue
+			}
+			return asciiVal
+		}
+	}	
+
+	// Error returns ""
+	fmt.Println("Seperation caracter not found.")
+	asciiVal = ""
+	return asciiVal
+}
 
 // fonction pour formater un message destiné à une autre application
 func msg_format(key string, val string) string {
@@ -38,7 +72,7 @@ func findval(tab_keyval []string, key string) string {
 
         tab_key_val := strings.Split(keyval[1:], keyval[0:1])
         if tab_key_val[0] == key {
-            val = return val
+            //val = return val
         }
     }
 	return val
@@ -53,6 +87,7 @@ func recaler(x, y int) int {
 }
 
 func main() {
+	fmt.Println(determinePairSep(":;aaa","bbb"))
 	var rcvmsg string
 	var tab_keyval []string // tableau de clefvaleur
 	var h int = 0 // horloge entière
