cd44844d848595815cc016efb51b855c60319f50
Elouan Geffrelot <elouan.geffrelot@etu.utc.fr>
2024-05-09 15:35:59 +0200

    Added many utility functions for game. Added file for utils funcs. Implemented player card drawing and card swappin + tested

--- app.go
+++ app.go
@@ -6,9 +6,9 @@ import (
 	"time"
 )
 
-///////////////////////////
-// GAMESTATE DECLARATION //
-///////////////////////////
+////////////////////////////
+// GAME STATE DECLARATION //
+////////////////////////////
 /*
 	Single Card structure
 */
@@ -31,7 +31,6 @@ type Player struct {
 	Allows to define the rules for the game
 */
 type GameSettings struct {
-	NumPlayers   int // Number of players
 	HandSize     int // Number of cards a player can hold
 	DrawPileSize int // Number of cards in the draw pile
 }
@@ -41,24 +40,24 @@ type GameSettings struct {
 	Contains deck, players (and their hands, score), draw pile
 */
 type GameState struct {
-	Settings GameSettings
-	Deck     []Card
-	Players  []Player
-	DrawPile []Card
+	Settings    GameSettings
+	Deck        []Card
+	Players     []Player
+	DrawPile    []Card
+	DiscardPile []Card
 }
 
-//////////////////////////////
-// GAMESTATE INITIALISATION //
-//////////////////////////////
+///////////////////////////////
+// GAME STATE INITIALISATION //
+///////////////////////////////
 /*
 	Returns initialised Settings
 */
 func getInitSettings() GameSettings {
 	logMessage("getInitSettings", "Initialising settings")
 	settings := GameSettings{
-		NumPlayers:   4,
-		HandSize:     4,
-		DrawPileSize: 4,
+		HandSize:     2,
+		DrawPileSize: 2,
 	}
 	return settings
 }
@@ -72,8 +71,10 @@ func getInitDeck() []Card {
 	var deck []Card
 
 	// Setting the deck building parameters
-	values := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
-	suits := []string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	//values := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
+	//suits := []string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	values := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	suits := []string{"Clubs", "Diamonds"}
 
 	// Building the deck
 	for _, suit := range suits {
@@ -111,10 +112,11 @@ func getInitPlayers() []Player {
 func getInitState() GameState {
 	logMessage("getInitState", "Initialising game state")
 	game := GameState{
-		Settings: getInitSettings(),
-		Deck:     getInitDeck(),
-		Players:  getInitPlayers(),
-		DrawPile: []Card{},
+		Settings:    getInitSettings(),
+		Deck:        getInitDeck(),
+		Players:     getInitPlayers(),
+		DrawPile:    []Card{},
+		DiscardPile: []Card{},
 	}
 	return game
 }
@@ -123,30 +125,162 @@ func getInitState() GameState {
 // GAME UTILS //
 ////////////////
 /*
-	Returns a game state with a new draw pile
+	Returns a gamestate with a new deck and empty discard pile
+	Puts the cards of the discard pile back into the deck and shuffles the deck
 */
-func renewDrawPile(game GameState) GameState {
-	logMessage("renewDrawPile", "Drawing cards from the deck to renew the draw pile.")
-	game.DrawPile = []Card{}
-	// Drawing cards from the deck until draw pile is filled
-	for i:=0; i<game.Settings.DrawPileSize; i++ {
-		// Adding card to the pile
-		game.DrawPile = append(game.DrawPile, game.Deck[len(game.Deck)-1])
-		// Removing added card from the deck
-		game.Deck = game.Deck[:len(game.Deck)-1]
-	}
+func reshuffleDiscard(game GameState) GameState {
+	logMessage("reshuffleDiscard", "Putting cards back from discard pile to deck and shuffling.")
 	
+	// Putting cards of the discard pile back in the deck
+	for i := 0; i < len(game.DiscardPile); i++ {
+		// Putting discard pile cards back in the deck
+		game.Deck = append(game.Deck, game.DiscardPile[i])
+	}
+
+	// Emptying discard pile
+	game.DiscardPile = []Card{}
+
+	// Shuffling the deck
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(game.Deck), func(i, j int) {
+		game.Deck[i], game.Deck[j] = game.Deck[j], game.Deck[i]
+	})
+
 	return game
 }
 
-/////////////////////////
-// GAME INITIALISATION //
-/////////////////////////
+/*
+	Returns a game state with a new draw pile
+	Works both with a renewing and a first draw of the draw pile 
+*/
+func renewDrawPile(game GameState) GameState {
+	// Bool allows to differentiate the first draw from the renewing of the pile
+	drawPileEmpty := len(game.DrawPile) == 0
+	if drawPileEmpty {
+		logMessage("renewDrawPile", "Drawing cards from the deck to fill the draw pile (first draw).")
+	} else {
+		logMessage("renewDrawPile", "Drawing cards from the deck to renew the draw pile.")
+	}
+
+	// Drawing cards from the deck until draw pile is filled
+	for i := 0; i < game.Settings.DrawPileSize; i++ {
+		// Reshuffling deck if it is empty
+		if len(game.Deck) == 0 {
+			game = reshuffleDiscard(game)
+		}
+
+		if drawPileEmpty == false {
+			// Renewing drawpile
+			// Adding old card to the discard pile
+			game.DiscardPile = append(game.DiscardPile, game.DrawPile[i])
+			// Replacing old card by new one in the draw pile
+			game.DrawPile[i] = game.Deck[len(game.Deck)-1]
+		} else {
+			// Setting empty draw pile
+			// Adding new card to the draw pile
+			game.DrawPile = append(game.DrawPile, game.Deck[len(game.Deck)-1])
+		}
+		// Removing added card from the deck
+		game.Deck = game.Deck[:len(game.Deck)-1]
+	}
+
+	return game
+}
+
+/*
+	Returns a game state with a new player hands
+	Works both with a renewing and a first draw of the player hands 
+*/
+func renewPlayerHands(game GameState) GameState {
+	// Bool allows to differentiate the first draw from the renewing of the pile
+	playerHandsEmpty := len(game.Players[0].Hand) == 0
+	if playerHandsEmpty {
+		logMessage("renewPlayerHands", "Drawing cards from the deck to fill the players hands (first draw).")
+	} else {
+		logMessage("renewPlayerHands", "Drawing cards from the deck to renew players hands.")
+	}
+
+	// Error returns same game state
+	if len(game.Players) == 0{
+		logError("renewPlayerHands", "Game does not have players.")
+		return game
+	}
+
+	// Drawing cards from the deck until player hands are filled
+	for i:= 0; i<game.Settings.HandSize; i++{
+		for playerIndex := 0; playerIndex < len(game.Players); playerIndex++ {
+			// Reshuffling deck if it is empty
+			if len(game.Deck) == 0 {
+				game = reshuffleDiscard(game)
+			}
+
+			if playerHandsEmpty == false {
+				// Renewing hands
+				// Adding old card to the discard pile
+				game.DiscardPile = append(game.DiscardPile, game.Players[playerIndex].Hand[i])
+				// Replacing old card by new one in the hand
+				game.Players[playerIndex].Hand[i] = game.Deck[len(game.Deck)-1]
+			} else {
+				// Setting empty hands
+				// Adding new card to the hand
+				game.Players[playerIndex].Hand = append(game.Players[playerIndex].Hand, game.Deck[len(game.Deck)-1])
+			}
+			// Removing added card from the deck
+			game.Deck = game.Deck[:len(game.Deck)-1]
+		}
+	}
+
+	return game
+}
+
+/*
+	Returns a game state after a player exchanged a card with the draw pile
+*/
+func swapCard(playerCard Card, drawPileCard Card, player Player, game GameState) GameState {
+	logMessage("swapCard", "Swapping player card "+toStringCard(playerCard)+" with draw pile card "+toStringCard(drawPileCard))
+	// Getting usefull variables and checking params
+	indexPlayer := findIndexPlayer(player, game.Players)
+	if indexPlayer == -1{
+		logError("swapCard", "Player " + toStringPlayer(player) + " does not exist.")
+		return game
+	}
+
+	playerHand := game.Players[indexPlayer].Hand
+	if contains(playerCard, playerHand) == false{
+		logError("swapCard", "Player " + toStringPlayer(player) + " does not have card " + toStringCard(playerCard))
+		return game
+	}
+
+	if (contains(drawPileCard,game.DrawPile) == false){
+		logError("swapCard", "Can't find card " + toStringCard(drawPileCard) + " in draw pile.")
+		return game
+	}
+
+	// The player hand and the drawpile both have the corresponding cards
+	// Changing players card 
+	game.Players[indexPlayer].Hand[findIndexCard(playerCard, playerHand)] = drawPileCard
+	// Changing draw pile card 
+	game.DrawPile[findIndexCard(drawPileCard, game.DrawPile)] = playerCard
+
+	logSuccess("swapCard", "Cards exchanged "+toStringCard(drawPileCard)+" with "+toStringCard(playerCard))
+	return game
+}
+
+//////////
+// GAME //
+//////////
 func app() {
 	// APP TESTS
 	gameState := getInitState()
 	fmt.Println(gameState)
 	gameState = renewDrawPile(gameState)
-	fmt.Println(gameState)
-	 
+	fmt.Println(gameState.DrawPile)
+	gameState = renewPlayerHands(gameState)
+	fmt.Println(gameState.Players)
+	fmt.Println("//////")
+	fmt.Println(gameState.DrawPile)
+	fmt.Println(gameState.Players[0])
+	gameState = swapCard(gameState.Players[0].Hand[0], gameState.DrawPile[0], gameState.Players[0], gameState)
+	fmt.Println(gameState.DrawPile)
+	fmt.Println(gameState.Players[0])
 }
