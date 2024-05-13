ea2f36c838696120c6b4b313bcf53ce4b11ce9c2
Elouan Geffrelot <elouan.geffrelot@etu.utc.fr>
2024-05-13 19:21:20 +0200

    Reverted to displaying to display only from the app that does the update

--- app.go
+++ app.go
@@ -382,8 +382,8 @@ func main() {
 				game = stringToGameState(messageReceived)
 				// Updated game state not sent anymore when update is received
 				// fmt.Printf(encodeMessage([]string{"snd", "msg"}, []string{name, gameStateToString(game)}) + "\n")
-				logInfo("main", "Updated game state (but did not diffuse the update.")
-				sendGameStateToPLayer(game) // Updating interface just in case went wrong last time
+				logInfo("main", "Updated game state (but did not diffuse the update nor display.")
+				// sendGameStateToPLayer(game) // Updating interface just in case went wrong last time
 			} else {
 				logSuccess("main", "Game state is already up to date, all apps up to date. (should not happen anymore)")
 			}
