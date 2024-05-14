b24f45e50db0d1a7eef1cffe9c92271e70b87dd5
Elouan Geffrelot <elouan.geffrelot@etu.utc.fr>
2024-05-14 12:06:57 +0200

    Getting better and better

--- io-utils.go
+++ io-utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -22,8 +23,11 @@ func write(ch <-chan string) {
 	for {
 		select {
 		case message := <-ch:
+			// Wait a random time between 10 and 40 ms and print next message
+			rand.Seed(time.Now().UnixNano())
+			duration := time.Duration(rand.Intn(31) + 10)
+			time.Sleep(duration * time.Millisecond)
 			fmt.Printf(message)
-			time.Sleep(10 * time.Millisecond)
 		}
 	}
 }
