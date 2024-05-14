1977a7323669fd639538c7273045d62f47fd0c46
Elouan Geffrelot <elouan.geffrelot@etu.utc.fr>
2024-05-14 13:19:26 +0200

    Added new booting system + rand seed initialisation + new delay in in-out go routines Stable version, didn't have noticable message loss

--- reseauComplet.sh
+++ reseauComplet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "Making named pipes"
 mkfifo /tmp/in_Debug
 
 mkfifo /tmp/in_A1 /tmp/out_A1
@@ -11,15 +11,28 @@ mkfifo /tmp/in_C2 /tmp/out_C2
 mkfifo /tmp/in_A3 /tmp/out_A3
 mkfifo /tmp/in_C3 /tmp/out_C3
  
+ echo "Starting A1"
 ./app -n A1 < /tmp/in_A1 >> /tmp/out_A1 &
+sleep 1
+echo "Starting C1"
 ./ctl -n C1 < /tmp/in_C1 >> /tmp/out_C1 &
+sleep 1
 
+echo "Starting A2"
 ./app -n A2 < /tmp/in_A2 >> /tmp/out_A2 &
+sleep 1
+echo "Starting C2"
 ./ctl -n C2 < /tmp/in_C2 >> /tmp/out_C2 &
+sleep 1
 
+echo "Starting A3"
 ./app -n A3 < /tmp/in_A3 >> /tmp/out_A3 &
+sleep 1
+echo "Starting C3"
 ./ctl -n C3 < /tmp/in_C3 >> /tmp/out_C3 &
- 
+sleep 1
+
+echo "Starting Network"
 cat /tmp/out_A1 | tee -a /tmp/in_C1 >> /tmp/in_Debug &
 cat /tmp/out_C1 | tee -a /tmp/in_A1 /tmp/in_C3 >> /tmp/in_C2 &
 
@@ -28,3 +41,5 @@ cat /tmp/out_C2 | tee -a /tmp/in_A2 /tmp/in_C1 >> /tmp/in_C3 &
 
 cat /tmp/out_A3 | tee -a /tmp/in_C3 >> /tmp/in_Debug &
 cat /tmp/out_C3 | tee -a /tmp/in_A3 /tmp/in_C2 >> /tmp/in_C1 &
+
+echo "Everything running. (start a display and a player to begin)"
