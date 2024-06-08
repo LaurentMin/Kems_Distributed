3b2bc85499d26d2cd70aadefe791c15db412095e
Elouan Geffrelot <elouan.geffrelot@gmail.com>
2024-06-08 15:49:41 +0200

    Forgot to add & to cats

--- NET/addNode.sh
+++ NET/addNode.sh
@@ -14,8 +14,8 @@ elif [ $# -eq 2 ]; then
     echo "Starting node..."
     ./net -n $1 -a $2 < /tmp/in_$1 >> /tmp/out_$1 &
     echo "Connecting named pipes..."
-    cat /tmp/out_$2 | tee -a /tmp/in_$1
-    cat /tmp/out_$1 | tee -a /tmp/in_$2
+    cat /tmp/out_$2 | tee -a /tmp/in_$1 &
+    cat /tmp/out_$1 | tee -a /tmp/in_$2 &
     echo "Node added to network."
 else
     echo "Error: Invalid number of arguments"
