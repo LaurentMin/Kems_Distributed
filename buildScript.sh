ee479d17667eb50a1f2fdcb442a37630ae901679
kilaposhi <kilapang@gamil.com>
2024-06-13 12:16:38 +0200

    Only one build script and sames clearscript

--- buildScript.sh
+++ buildScript.sh
@@ -11,4 +11,8 @@ go build -o terminal-display terminal-display.go app-utils.go utils.go logs.go c
 echo "Built display"
 go build -o web-proxy web-proxy.go app-utils.go utils.go logs.go client-utils.go io-utils.go
 echo "Built web-proxy"
+
+echo "Building NET..."
+cd ./NET/
+go build -o net net.go net-utils.go pastouche3.go pastouche2.go pastouche1.go
 echo "Done."
