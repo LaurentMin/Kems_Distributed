5ed683849a3d5a2f357574cdf6c2556d64ea5881
Elouan Geffrelot <elouan.geffrelot@etu.utc.fr>
2024-06-13 12:05:32 +0200

    Modified file for the K's graph part

--- NET/save2.sh
+++ NET/save2.sh
@@ -5,12 +5,9 @@ killall tee 2> /dev/null
 killall cat 2> /dev/null
 
 echo "Making named pipes..."
+
 mkfifo /tmp/in_A3 /tmp/out_A3
 mkfifo /tmp/in_C3 /tmp/out_C3
-
-mkfifo /tmp/in_D1
-mkfifo /tmp/in_D2
-mkfifo /tmp/in_D3
 
 mkfifo /tmp/in_N3 /tmp/out_N3
 
@@ -19,13 +16,13 @@ cat /tmp/out_N1 | tee -a /tmp/in_N2 /tmp/in_N3 >> /tmp/in_C1 &
 cat /tmp/out_N2 | tee -a /tmp/in_N1 >> /tmp/in_C2 &
 cat /tmp/out_N3 | tee -a /tmp/in_N1 >> /tmp/in_C3 &
 
-cat /tmp/out_A1 | tee -a /tmp/in_C1 /tmp/in_D1 >> /tmp/in_Debug &
+cat /tmp/out_A1 | tee -a /tmp/in_C1 >> /tmp/in_Debug &
 cat /tmp/out_C1 | tee -a /tmp/in_A1 >> /tmp/in_N1 &
 
-cat /tmp/out_A2 | tee -a /tmp/in_C2 /tmp/in_D2 >> /tmp/in_Debug &
+cat /tmp/out_A2 | tee -a /tmp/in_C2 >> /tmp/in_Debug &
 cat /tmp/out_C2 | tee -a /tmp/in_A2 >> /tmp/in_N2 &
 
-cat /tmp/out_A3 | tee -a /tmp/in_C3 /tmp/in_D3 >> /tmp/in_Debug &
+cat /tmp/out_A3 | tee -a /tmp/in_C3 >> /tmp/in_Debug &
 cat /tmp/out_C3 | tee -a /tmp/in_A3 >> /tmp/in_N3 &
 
 echo "Starting App..."
@@ -36,5 +33,6 @@ echo "Starting Controller..."
 sleep 1
 echo "Starting network node..."
 ./net -n N3 -a N1 < /tmp/in_N3 >> /tmp/out_N3 &
+
 
 echo "Network built."
